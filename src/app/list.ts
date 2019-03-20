17b721b960239a1b3aa5aad4d7b9a3979ee8bfe3
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-20 11:29:25 +0300

    Added date pipe

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,6 +1,6 @@
 export class List {
   public showDescription:boolean;
-  constructor(public id:number,public title:string ,public descriptions:string,public date:date){
+  constructor(public id:number,public title:string ,public descriptions:string,public date:Date){
     this.showDescription=false;
   };
 
